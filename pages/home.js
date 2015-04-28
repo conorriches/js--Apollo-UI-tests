2a2b6f0089478fdaf5c38e7a1cc9049b8710e3d9
Vasil Vanchuk <v.vanchuk@tut.by>
2015-04-28 16:01:02 +0300

    Patch alert/confirm before showing on remove group

--- pages/home.js
+++ pages/home.js
@@ -20,8 +20,8 @@ module.exports = {
     return this.client
       .moveToElement(SELECTOR_ADD_OBJECT_BTN, 10, 10)
       .click(SELECTOR_ADD_OBJECT_BTN)
-      .assert.elementPresent('.modal-title')
-      .assert.containsText('.modal-title', 'Add Object')
+      .assert.elementPresent('.modal-title', 'Should show modal')
+      .assert.containsText('.modal-title', 'Add Object', 'Modal title shoul be "Add object"')
       .click(SELECTOR_ADD_OBJECT_FORM_TITLE)
       .setValue(SELECTOR_ADD_OBJECT_FORM_TITLE, objectName)
       .moveToElement(SELECTOR_MODAL_SUBMIT_BTN, 10, 10)
@@ -29,7 +29,7 @@ module.exports = {
       .waitForElementNotPresent('.spinner')
       .waitForElementPresent('.info-block-text h2 i')
       // todo: add urlMatch assertion
-      .assert.urlContains('objects/object')
+      .assert.urlContains('objects/object', "Should be redirected into object page")
       .assert.urlContains('description')
       .assert.elementPresent('.info-block-text h2 i')
       .assert.containsText('.info-block-text h2 i', objectName, 'Element for object "' + objectName + '" was found')
@@ -52,7 +52,7 @@ module.exports = {
   createSection: function(sectionName) {
     return this.client
       .click('.top-section-home .pull-right .btn:first-of-type')
-      .assert.elementPresent('.modal-content')
+      .assert.elementPresent('.modal-content', "Should open modal")
       .click('.modal-body input[name=name]')
       .setValue('.modal-body input[name=name]', sectionName)
       .click('.modal-footer .btn-primary')
@@ -70,7 +70,7 @@ module.exports = {
 
     return this.client
       .click('.top-section-home .pull-right .btn:nth-child(2)')
-      .assert.elementPresent('.modal-content')
+      .assert.elementPresent('.modal-content', "Should open modal")
       .click('.modal-body input[name=name]')
       .setValue('.modal-body input[name=name]', groupName)
       .click('.modal-body input[name=description]')
@@ -109,15 +109,15 @@ module.exports = {
     var client = this.client;
 
     return this.client
+      .jqueryClick(jqSelector)
+      .waitForElementNotPresent('.spinner')
+      .waitForElementPresent('.top-section .btn')
       .execute(function() {
         // patch for phantomjs
         // todo: create compatibility file
         window.confirm = function(){return true;};
         window.alert = function(){return true;};
       })
-      .jqueryClick(jqSelector)
-      .waitForElementNotPresent('.spinner')
-      .waitForElementPresent('.top-section .btn')
       .jqueryClick('.top-section .btn:contains("Delete Group")')
       .acceptAlert()
       .waitForElementNotPresent('.spinner')
